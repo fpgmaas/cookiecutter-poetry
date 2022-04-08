e0b64bcac6d14870596ab14ac09bea2223409e1f
Florian Maas <florian.maas@ingka.ikea.com>
2022-04-08 17:15:57 +0200

    ifixed post-hook and made github actions optional (#2)

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import shutil
+
+PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
+
+
+def remove_file(filepath):
+    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
+
+
+def remove_dir(filepath):
+    shutil.rmtree(os.path.join(PROJECT_DIRECTORY, filepath))
+
+
+if __name__ == "__main__":
+
+    if "{{cookiecutter.include_pyenv_local_file}}" != "y":
+        remove_file(".python-version")
+
+    if "{{cookiecutter.include_github_actions}}" != "y":
+        remove_dir(".github")
