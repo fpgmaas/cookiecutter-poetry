4feafcda6cc284e4e488326a2940d7973119bf75
Florian Maas <florian.maas@ingka.ikea.com>
2022-04-08 16:47:46 +0200

    fixed issue with black, applied formatting

--- hooks/post_gen_python.py
+++ hooks/post_gen_python.py
@@ -8,10 +8,10 @@ def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
-    if '{{cookiecutter.pyenv_local_file}}' != 'y':
-        remove_file('python-version')
+    if "{{cookiecutter.pyenv_local_file}}" != "y":
+        remove_file("python-version")
 
-    if 'Not open source' == '{{cookiecutter.open_source_license}}':
-        remove_file('LICENSE')
+    if "Not open source" == "{{cookiecutter.open_source_license}}":
+        remove_file("LICENSE")
