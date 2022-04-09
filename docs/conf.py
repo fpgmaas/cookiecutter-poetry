ed183c1882bf9a6179402a97f49bd91e94c497b5
Florian Maas <florian.maas@ingka.ikea.com>
2022-04-09 22:47:28 +0200

    removed _static

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,3 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "alabaster"
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
