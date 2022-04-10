119b9dc57a505c06f292c1f8a8da4d40844de85e
Florian Maas <florian.maas@ingka.ikea.com>
2022-04-10 15:34:48 +0200

    formatting

--- docs/conf.py
+++ docs/conf.py
@@ -38,12 +38,12 @@ templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # These folders are copied to the documentation's HTML output
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
-    'css/custom.css',
+    "css/custom.css",
 ]
 
 # -- Options for HTML output -------------------------------------------------
