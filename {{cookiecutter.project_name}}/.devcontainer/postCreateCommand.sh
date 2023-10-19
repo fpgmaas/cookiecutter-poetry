3788593a3f6d7998c4ed44a587ef91c7d4c4444a
Ken Brewer <kenibrewer@users.noreply.github.com>
2023-10-19 14:19:49 +0000

    Removed initialization code

--- {{cookiecutter.project_name}}/.devcontainer/postCreateCommand.sh
+++ {{cookiecutter.project_name}}/.devcontainer/postCreateCommand.sh
@@ -3,17 +3,5 @@
 # Install Dependencies
 poetry install --with dev
 
-# Check if the repo is already initialized
-if [ ! -d ".git" ]; then
-    # Initialize the repo
-    git init -b main
-    git add .
-    # Run pre-commit hooks on all files
-    poetry run pre-commit install --install-hooks
-    poetry run pre-commit run --all-files
-    # Stage modified files
-    git add .
-else
-    # Install pre-commit hooks
-    poetry run pre-commit install --install-hooks
-fi
+# Install pre-commit hooks
+poetry run pre-commit install --install-hooks
