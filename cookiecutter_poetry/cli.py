e8ebf4f6a1d7b70cd6808d0808d14c9d90082fa0
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2024-07-02 21:03:21 +0200

    Handle Bandit issues in pre-commit

--- cookiecutter_poetry/cli.py
+++ cookiecutter_poetry/cli.py
@@ -4,4 +4,4 @@ import os
 def main() -> None:
     cwd = os.path.dirname(__file__)
     package_dir = os.path.abspath(os.path.join(cwd, ".."))
-    os.system(f"cookiecutter {package_dir}")
+    os.system(f"cookiecutter {package_dir}")  # noqa: S605 | No injection, retrieving path in OS
