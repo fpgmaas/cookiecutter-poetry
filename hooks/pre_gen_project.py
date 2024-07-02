8134f27936e41e2e27bbca5415030d5834cb953e
Florian Maas <fpgmaas@gmail.com>
2024-07-02 22:07:15 +0200

    Bump ruff (#114)
    * bump ruff

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -5,8 +5,7 @@ PROJECT_NAME_REGEX = r"^[-a-zA-Z][-a-zA-Z0-9]+$"
 project_name = "{{cookiecutter.project_name}}"
 if not re.match(PROJECT_NAME_REGEX, project_name):
     print(
-        "ERROR: The project name (%s) is not a valid Python module name. Please do not use a _ and use - instead"
-        % project_name
+        f"ERROR: The project name {project_name} is not a valid Python module name. Please do not use a _ and use - instead"
     )
     # Exit to cancel project
     sys.exit(1)
@@ -15,8 +14,7 @@ PROJECT_SLUG_REGEX = r"^[_a-zA-Z][_a-zA-Z0-9]+$"
 project_slug = "{{cookiecutter.project_slug}}"
 if not re.match(PROJECT_SLUG_REGEX, project_slug):
     print(
-        "ERROR: The project slug (%s) is not a valid Python module name. Please do not use a - and use _ instead"
-        % project_slug
+        f"ERROR: The project slug {project_slug} is not a valid Python module name. Please do not use a - and use _ instead"
     )
     # Exit to cancel project
     sys.exit(1)
